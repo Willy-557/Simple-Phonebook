1f0d0f4bc0bccdfa4d63e17b257eaa4cf42dbf3e
Willy-557 <williamfsetiadi@gmail.com>
2026-01-07 13:23:36 +0700

    Finish the case 1

--- FilePhonebook.java
+++ FilePhonebook.java
@@ -9,10 +9,23 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 class Kontak{
-    String nama;
     String nomorHP;
     String email;
     String grup;
+    String status;
+
+    public Kontak(String nomorHP, String email, String grup, String status){
+        this.nomorHP = nomorHP;
+        this.email = email;
+        this.grup = grup;
+        this.status = status;
+    }
+
+
+
+
+
+
 
     
 }
@@ -25,15 +38,46 @@ public class FilePhonebook {
         
         while (true){
             System.out.println("----- PhoneBook -----");
-            System.out.println("1. Cari kontak berdasarkan nama");
-            System.out.println("2. Telepon kontak");
-            System.out.println("3. Ubah status kontak");
-            System.out.println("4. Keluar");
+            System.out.println("1. Tambah kontak");
+            System.out.println("2. Cari kontak berdasarkan nama");
+            System.out.println("3. Telepon kontak");
+            System.out.println("4. Ubah status kontak");
+            System.out.println("5. Keluar");
             System.out.print(">> ");
             int opsi = scanner.nextInt();
 
-            if (opsi == 4){
+            if (opsi == 5){
                 System.out.println();
+            }
+            else if (opsi > 5 || opsi < 0){
+                System.out.println("Invalid input!");
+                continue;
+            }
+            else {
+                switch (opsi) {
+                    case 1:
+                        scanner.nextLine();
+                        System.out.print("Masukkan nama: ");
+                        String namaKontak = scanner.nextLine();
+
+                        System.out.print("Masukkan nomor HP: ");
+                        String noHP = scanner.nextLine();
+
+                        System.out.print("Masukkan email: ");
+                        String emailKontak = scanner.nextLine();
+                        
+                        System.out.print("Grup (Keluarga / Kantor): ");
+                        String grupKontak = scanner.nextLine();
+
+                        String statusKontak = "Belum di-blokir";
+
+                        Kontak dataKontakBaru = new Kontak(noHP, emailKontak, grupKontak, statusKontak);
+                        
+                        penyimpananKontak.put(namaKontak, dataKontakBaru);
+                        System.out.println("Berhasil menyimpan kontak bernama '" + namaKontak + "'.");
+                        break;
+                
+                }
             }
 
 
